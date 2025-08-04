9cf5fa5752df3b49405fdcbb196ae6853cc75fc4
Clarke <a.clarke@MacBookPro.lan>
2025-08-04 12:21:46 +0100

    moved ajm/potential_fix plus db fixes to main, leaving ajm/potential_fix unchanged in case

--- platform/src-posix-mapper/db-init/01-init-schema.sql
+++ platform/src-posix-mapper/db-init/01-init-schema.sql
@@ -1,20 +1,20 @@
 -- Create the schema for the posix mapper
-CREATE SCHEMA posixmap;
+CREATE SCHEMA IF NOT EXISTS posixmap;
 GRANT ALL ON SCHEMA posixmap TO cadmin;
 
 -- Create the necessary tables within the schema
-CREATE TABLE posixmap.users (
+CREATE TABLE IF NOT EXISTS posixmap.users (
     username VARCHAR(255) PRIMARY KEY,
     uid INTEGER UNIQUE NOT NULL,
     primary_gid INTEGER NOT NULL
 );
 
-CREATE TABLE posixmap.groups (
+CREATE TABLE IF NOT EXISTS posixmap.groups (
     groupname VARCHAR(255) PRIMARY KEY,
     gid INTEGER UNIQUE NOT NULL
 );
 
-CREATE TABLE posixmap.group_membership (
+CREATE TABLE IF NOT EXISTS posixmap.group_membership (
     uid INTEGER REFERENCES posixmap.users(uid),
     gid INTEGER REFERENCES posixmap.groups(gid),
     PRIMARY KEY (uid, gid)
